408fa4ca9981550828d3e78aa08e2b1e789ae863
aXXyss <axxyss@gmail.com>
2022-10-06 18:27:21 +0200

    entorno produccion

--- GestExport/settings.py
+++ GestExport/settings.py
@@ -147,9 +147,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = '/var/www/html/GestExport/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
 # Default primary key field type
--- GestExport/wsgi.py
+++ GestExport/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+
+path = os.path.expanduser('~/GestExport')
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'GestExport.settings'
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GestExport.settings')
-
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
